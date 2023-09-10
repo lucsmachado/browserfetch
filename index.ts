9d2e16796a2c8c4f1ac05b863dea1f3fd85bdf93
Lucas Machado <lcmachado.cs@gmail.com>
2023-09-10 14:42:11 -0300

    Parse User-Agent header

--- index.ts
+++ index.ts
@@ -1,60 +1,66 @@
-type Client = {
+type ProductIdentifier = {
 	name: string;
-	version: string;
+	version?: string;
+	comments: string[];
 }
 
-const userAgents = new Array<string>();
+type UserAgentData = {
+	products: ProductIdentifier[];
+}
 
-const clients = new Map<string, number>();
+function parseUserAgent(userAgentString: string): UserAgentData {
+	const data: UserAgentData = {
+		products: [],
+	};
+
+	const regex = /([^/;]+)(?:\/([\w.]+))?(?:\s*\(([^)]+)\))?/g;
+
+	let match;
+	while ((match = regex.exec(userAgentString)) !== null) {
+		const name = match[1].trim();
+		const version = match[2] ? match[2].trim() : undefined;
+		const comment = match[3] ? match[3].trim() : undefined;
+
+		if (data.products.length === 0 || version) {
+			const newProduct: ProductIdentifier = {
+				name,
+				version,
+				comments: comment ? [comment] : [],
+			};
+
+			data.products.push(newProduct);
+		} else {
+			const lastProduct = data.products.at(-1);
+			if (lastProduct && comment) {
+				lastProduct.comments.push(comment);
+			}
+		}
+	}
+
+	return data;
+}
 
 const server = Bun.serve({
 	port: 3000,
 	fetch(req) {
 		const url = new URL(req.url);
 		if (url.pathname === '/') {
-			const headers = req.headers.toJSON();
-			const reqUserAgent = headers["user-agent"];
-			userAgents.push(reqUserAgent);
+			const userAgentString = req.headers.get("User-Agent");
 
-			clients.set(reqUserAgent, (clients.get(reqUserAgent) || 0) + 1);
-
-			const regex = /^(\w+)\/([\d.]+)/;
-			const match = reqUserAgent.match(regex);
-
-			if (match) {
-				const client: Client = {
-					name: match[1],
-					version: match[2],
-				}
-				return new Response(JSON.stringify(client), {
+			if (userAgentString) {
+				const data = parseUserAgent(userAgentString);
+				return new Response(JSON.stringify(data), {
 					headers: {
 						'Content-Type': 'application/json',
 					},
 				});
+			} else {
+				return new Response(undefined, {
+					status: 400,
+				});
 			}
-			return new Response(JSON.stringify({ userAgent: reqUserAgent }), {
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			});
 		}
-		if (url.pathname === '/count') {
-			const headers = req.headers.toJSON();
-			const reqUserAgent = headers["user-agent"];
-			userAgents.push(reqUserAgent);
-
-			clients.set(reqUserAgent, (clients.get(reqUserAgent) || 0) + 1);
-			const reqCount = clients.get(reqUserAgent);
-			const res = {
-				requests: reqCount,
-			};
-			return new Response(JSON.stringify(res), {
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			});
-		}
-		return new Response("Not found", {
+		return new Response(undefined, {
 			status: 404,
 		});
 	},
